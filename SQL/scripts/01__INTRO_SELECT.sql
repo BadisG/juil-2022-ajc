f9e80eabefbba91ba21a57d0277843c5a9e72fd6
Jeremy <jeremy.perrouault@ascadis.fr>
2022-08-01 11:20:51 +0200

    SQL - SUPPORT & SCRIPTS SQL INIT DB

---
+++ SQL/scripts/01__INTRO_SELECT.sql
@@ -0,0 +1,15 @@
+-- Sélectionner tous les clients
+SELECT * FROM client;
+
+-- Sélectionner le nom et le prix de tous les produits
+SELECT pro_nom, pro_prix_vente FROM produit;
+
+-- Sélectionner toutes les commandes
+SELECT * FROM commande;
+
+
+-- Sélectionner tous les nom & prénom des clients dans une seule colonne
+-- -> Les afficher en MAJUSCULES
+
+
+-- Sélectionner le mois et l'année (mois-annee) des commandes
