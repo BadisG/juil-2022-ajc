853d5f1222960787a219ad8589611e8acf1a4716
Jeremy <jeremy.perrouault@ascadis.fr>
2022-08-01 12:06:43 +0200

    JOUR 1 - INTRO SELECT

--- SQL/scripts/01__INTRO_SELECT.sql
+++ SQL/scripts/01__INTRO_SELECT.sql
@@ -10,6 +10,22 @@ SELECT * FROM commande;
 
 -- Sélectionner tous les nom & prénom des clients dans une seule colonne
 -- -> Les afficher en MAJUSCULES
+SELECT UPPER(CONCAT(cli_nom, ' ', cli_prenom)) FROM client;
+
+SELECT UPPER(cli_nom||' '||cli_prenom) FROM client;
 
 
 -- Sélectionner le mois et l'année (mois-annee) des commandes
+SELECT
+	COALESCE(
+		DATE_PART('MONTH', cmd_date)||'-'||DATE_PART('YEAR', cmd_date),
+      	'0'
+    )
+FROM commande;
+
+SELECT
+	COALESCE(cmd_date, '2022-08-01')
+FROM commande;
+
+SELECT COALESCE('Test pas null', 'demo');
+SELECT COALESCE(NULL, 'demo');
