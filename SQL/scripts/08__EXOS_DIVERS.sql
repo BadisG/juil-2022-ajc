e5be3568b0b91b3ca874dc087b63fdc119760976
Jeremy <jeremy.perrouault@ascadis.fr>
2022-08-02 09:41:31 +0200

    Renommage exemple -> relation

---
+++ SQL/scripts/08__EXOS_DIVERS.sql
@@ -0,0 +1,35 @@
+-- Nom et montant de toutes les promotions
+
+-- Nom et prénom du client #2
+
+-- Nom, prix d'achat, prix de vente du produit #1
+
+-- Nom du fournisseur #1 et noms de ses produits
+
+-- Compter le nombre de promotions
+
+-- Le nombre de commandes pour chaque client_id trié par nombre décroissant
+
+-- Nombre de produits par fournisseur (nom fournisseur et nb produits)
+
+-- Somme de tous les montants (des achats)
+
+-- Moyenne des prix de vente des produits
+
+-- Montant minimum des achats
+
+-- Prix de vente maximum des produits
+
+-- Première date de commande, dernière date de commande
+
+-- Compter les commandes par client, et afficher le nb, le nom et le prénom du client
+
+-- Afficher le nom du produit, le prix de vente d'un produit avec sa promotion (prixvente - montant promo)
+-- > Afficher tous les prix de tous les produits
+
+-- Afficher le nom du client et son / ses commentaires (commentaire, et la note)
+
+-- Compter le nombre de commentaire pour chaque client, avec le nom du client
+
+-- Lister les produits (nom, prix de vente)
+-- > Dont le prix de vente est inférieur au prix vendu (montant de achat)
