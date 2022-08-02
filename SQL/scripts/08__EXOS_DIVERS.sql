a3aa8be9db51b9495f8aabee7af15087ab2a5bd8
Jeremy <jeremy.perrouault@ascadis.fr>
2022-08-02 10:04:29 +0200

    JOUR 2 - EXOS DIVERS

--- SQL/scripts/08__EXOS_DIVERS.sql
+++ SQL/scripts/08__EXOS_DIVERS.sql
@@ -1,24 +1,66 @@
 -- Nom et montant de toutes les promotions
+SELECT pmt_nom, pmt_montant FROM promotion;
 
 -- Nom et prénom du client #2
+SELECT cli_nom, cli_prenom FROM client WHERE cli_id = 2;
 
 -- Nom, prix d'achat, prix de vente du produit #1
+SELECT pro_nom, pro_prix_achat, pro_prix_vente FROM produit WHERE pro_id = 1;
 
 -- Nom du fournisseur #1 et noms de ses produits
+SELECT
+    fou_nom,
+    pro_nom
+FROM fournisseur
+LEFT JOIN produit ON pro_fournisseur_id = fou_id
+WHERE fou_id = 1;
 
 -- Compter le nombre de promotions
+SELECT COUNT(pmt_id) AS nb FROM promotion;
 
 -- Le nombre de commandes pour chaque client_id trié par nombre décroissant
+SELECT
+    COUNT(cmd_id) AS nb,
+    cmd_client_id
+FROM commande
+GROUP BY cmd_client_id
+-- ORDER BY 1 DESC
+ORDER BY nb DESC
+;
 
 -- Nombre de produits par fournisseur (nom fournisseur et nb produits)
+SELECT
+    COUNT(pro_id) AS nb,
+    fou_nom
+FROM produit
+RIGHT JOIN fournisseur ON fou_id = pro_fournisseur_id
+GROUP BY fou_id;
+
+SELECT
+    COUNT(pro_id) AS nb,
+    fou_nom
+FROM fournisseur
+LEFT JOIN produit ON fou_id = pro_fournisseur_id
+GROUP BY fou_id;
 
 -- Somme de tous les montants (des achats)
+SELECT SUM(ach_montant * ach_quantite) FROM achat;
 
 -- Moyenne des prix de vente des produits
+SELECT AVG(pro_prix_vente) FROM produit;
 
 -- Montant minimum des achats
+SELECT MIN(ach_montant * ach_quantite) FROM achat;
 
 -- Prix de vente maximum des produits
+SELECT MAX(pro_prix_vente) FROM produit;
+
+
+
+
+
+
+
 
 -- Première date de commande, dernière date de commande
 
