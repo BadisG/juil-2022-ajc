8c19e28de0bf371149cac34fd6e8b3049b47c7b2
Jeremy <jeremy.perrouault@ascadis.fr>
2022-08-23 16:23:24 +0200

    JAVA AVANCE - JOUR 2 - Factorisation

--- JAVA/eshop-java/src/fr/formation/repo/sql/ProduitRepositorySql.java
+++ JAVA/eshop-java/src/fr/formation/repo/sql/ProduitRepositorySql.java
@@ -13,20 +13,69 @@ import fr.formation.model.Produit;
 import fr.formation.repo.IProduitRepository;
 
 public class ProduitRepositorySql implements IProduitRepository {
+	private Connection connection = null;
+	
+	// Méthode pour se connecter
+	private Connection connect() throws SQLException {
+		// 1- Se connecter au serveur SGBD
+		this.connection = DriverManager.getConnection("jdbc:postgresql://127.0.0.1:5432/eshop", "postgres", "root");
+		
+		return this.connection;
+	}
+	
+	// Méthode pour exécuter une requête SQL
+	private PreparedStatement prepare(String query) throws SQLException {
+//		this.connect();
+//		return this.connection.prepareStatement(query);
+		
+		return this.connect().prepareStatement(query);
+	}
+	
+	// Méthode pour se déconnecter
+	private void disconnect() {
+		if (this.connection != null) {
+			try {
+				this.connection.close();
+			}
+			
+			catch (SQLException e) {
+				System.out.println("La déconnexion n'a pas fonctionné.");
+			}
+		}
+	}
+	
+	// Méthode pour générer les produits / fournisseurs
+	private Produit map(ResultSet myResult) {
+		try {
+			// Pour chaque résultat, il faudra créer un nouveau Produit (java) et un nouveau Fournisseur
+			Produit monProduit = new Produit();
+			Fournisseur monFournisseur = new Fournisseur();
+			
+			// On associe toutes les informations du produit
+			monProduit.setId( myResult.getInt("pro_id") );
+			monProduit.setNom( myResult.getString("pro_nom") );
+			monProduit.setPrixAchat( myResult.getFloat("pro_prix_achat") );
+			monProduit.setPrixVente( myResult.getFloat("pro_prix_vente") );
+			
+			// On associe toutes les infos du fournisseur
+			monFournisseur.setId( myResult.getInt("pro_fournisseur_id") );
+			
+			// On associe le fournisseur au produit
+			monProduit.setFournisseur(monFournisseur);
+			
+			return monProduit;
+		}
+		
+		catch (SQLException e) {
+			return null;
+		}
+	}
+	
+	
 	@Override
 	public Produit findById(Integer id) {
-		Connection myConnection = null;
-		
 		try {
-			// 1- Se connecter au serveur SGBD
-			
-			// URL de connexion = jdbc:pilote://hote:port/base_de_donnees (jdbc = Java DataBase Connection)
-			myConnection = DriverManager.getConnection("jdbc:postgresql://127.0.0.1:5432/eshop", "postgres", "root");
-			
-			// 2- Créer un Statement pour exécuter une requête SQL
-			
-			// On va utiliser une requête préparée pour éviter les injections SQL pour simplifier la syntaxe
-			PreparedStatement myStatement = myConnection.prepareStatement("SELECT * FROM produit WHERE pro_id = ?");
+			PreparedStatement myStatement = this.prepare("SELECT * FROM produit WHERE pro_id = ?");
 			
 			// On injecte les paramètres à la requête (l'index commence à 1)
 			myStatement.setInt(1, id);
@@ -38,21 +87,7 @@ public class ProduitRepositorySql implements IProduitRepository {
 			
 			// 4- Parcourir le ResultSet
 			if (myResult.next()) {
-				// Pour chaque résultat, il faudra créer un nouveau Produit (java) et un nouveau Fournisseur
-				monProduit = new Produit();
-				Fournisseur monFournisseur = new Fournisseur();
-				
-				// On associe toutes les informations du produit
-				monProduit.setId( myResult.getInt("pro_id") );
-				monProduit.setNom( myResult.getString("pro_nom") );
-				monProduit.setPrixAchat( myResult.getFloat("pro_prix_achat") );
-				monProduit.setPrixVente( myResult.getFloat("pro_prix_vente") );
-				
-				// On associe toutes les infos du fournisseur
-				monFournisseur.setId( myResult.getInt("pro_fournisseur_id") );
-				
-				// On associe le fournisseur au produit
-				monProduit.setFournisseur(monFournisseur);
+				monProduit = this.map(myResult);
 			}
 			
 			return monProduit;
@@ -63,16 +98,7 @@ public class ProduitRepositorySql implements IProduitRepository {
 		}
 		
 		finally {
-			if (myConnection != null) {
-				try {
-					// 5- Fermeture de la connexion
-					myConnection.close();
-				}
-				
-				catch (SQLException e) {
-					System.out.println("Impossible de fermer la connexion.");
-				}
-			}
+			this.disconnect();
 		}
 		
 		return null;
@@ -82,39 +108,15 @@ public class ProduitRepositorySql implements IProduitRepository {
 	public List<Produit> findAll() {
 		List<Produit> produits = new ArrayList<>();
 		
-		Connection myConnection = null;
-		
 		try {
-			// 1- Se connecter au serveur SGBD
-			
-			// URL de connexion = jdbc:pilote://hote:port/base_de_donnees (jdbc = Java DataBase Connection)
-			myConnection = DriverManager.getConnection("jdbc:postgresql://127.0.0.1:5432/eshop", "postgres", "root");
-			
-			// 2- Créer un Statement pour exécuter une requête SQL
-			
-			// On va utiliser une requête préparée pour éviter les injections SQL pour simplifier la syntaxe
-			PreparedStatement myStatement = myConnection.prepareStatement("SELECT * FROM produit");
+			PreparedStatement myStatement = this.prepare("SELECT * FROM produit");
 			
 			// 3- Récupérer le résultat du SELECT, et le stocker dans un ResultSet
 			ResultSet myResult = myStatement.executeQuery();
 			
 			// 4- Parcourir le ResultSet
 			while (myResult.next()) {
-				// Pour chaque résultat, il faudra créer un nouveau Produit (java) et un nouveau Fournisseur
-				Produit monProduit = new Produit();
-				Fournisseur monFournisseur = new Fournisseur();
-				
-				// On associe toutes les informations du produit
-				monProduit.setId( myResult.getInt("pro_id") );
-				monProduit.setNom( myResult.getString("pro_nom") );
-				monProduit.setPrixAchat( myResult.getFloat("pro_prix_achat") );
-				monProduit.setPrixVente( myResult.getFloat("pro_prix_vente") );
-				
-				// On associe toutes les infos du fournisseur
-				monFournisseur.setId( myResult.getInt("pro_fournisseur_id") );
-				
-				// On associe le fournisseur au produit
-				monProduit.setFournisseur(monFournisseur);
+				Produit monProduit = this.map(myResult);
 				
 				// On ajoute le produit à la liste
 				produits.add(monProduit);
@@ -126,16 +128,7 @@ public class ProduitRepositorySql implements IProduitRepository {
 		}
 		
 		finally {
-			if (myConnection != null) {
-				try {
-					// 5- Fermeture de la connexion
-					myConnection.close();
-				}
-				
-				catch (SQLException e) {
-					System.out.println("Impossible de fermer la connexion.");
-				}
-			}
+			this.disconnect();
 		}
 		
 		return produits;
